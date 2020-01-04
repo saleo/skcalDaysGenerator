ed2c4a8ebdc2be8a3ddd41e5436a13260fbe872d
leoliu <leo@euse.net>
2020-01-04 19:24:09 +0800

    fix bug:color on 月晦,雷斋日,天地仓开日,南赡部洲击大法鼓,金龙四大王诞; add grey for 宜戒(gray)

--- public/getByLunarYear.js
+++ public/getByLunarYear.js
@@ -4,6 +4,7 @@ let lbc = require("lunar-birthday-calendar");
 let eventsByLunar= require("./moduleEventsByLunar.js")
 var i=0,len;
 var color="red",argsArray = [];
+let blueItems={"三元降","望","朔","太素三元君朝真","月晦","雷斋日","天地仓开日","天地交道","南赡部洲击大法鼓","金龙四大王诞"};
 
 filename=process.argv[2];
 
@@ -20,19 +21,22 @@ csv.fromPath("./"+filename+".csv", {
         for (i=0;i<len;i++) {     
             let color="red";
             let summary=eventsArray[i];
-            if (summary=="三元降"||summary=="望"||summary=="朔"||summary=="太素三元君朝真")
-                    color="blue";
-    
-                let args = {      
-                    solar_year: moment(data.solar, 'MM/DD/YYYY').format('Y'),
-                    solar_month: moment(data.solar, 'MM/DD/YYYY').format('M'),
-                    solar_day: moment(data.solar, 'MM/DD/YYYY').format('D'),
-                    name: summary,
-                    color: color,
-                    count: 1
-                }
-                argsArray.push(args)
+            if (blueItems.indexOf(summary)>-1)
+                color="blue";
+
+            if (summary=="宜戒")                    
+                color="gray";
+
+            let args = {      
+                solar_year: moment(data.solar, 'MM/DD/YYYY').format('Y'),
+                solar_month: moment(data.solar, 'MM/DD/YYYY').format('M'),
+                solar_day: moment(data.solar, 'MM/DD/YYYY').format('D'),
+                name: summary,
+                color: color,
+                count: 1
             }
+            argsArray.push(args)
+        }   
     }
 }).on("end", function() {
     let ical = lbc.generateCalendarWithSolar(argsArray) ;
